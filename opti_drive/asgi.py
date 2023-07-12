9eb9fd2f4c53da76af64e2f544c7b1fe3ca3976c
msterhuj <gabin.lanore@gmail.com>
2023-07-13 00:00:59 +0200

    fix: wsgi loader

--- opti_drive/asgi.py
+++ opti_drive/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "OptiDrive.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "opti_drive.settings")
 
 application = get_asgi_application()
