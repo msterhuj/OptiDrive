0587c114df2c863e94b1c3b505ffcfd6f2542068
msterhuj <gabin.lanore@gmail.com>
2023-07-13 12:02:55 +0200

    feat: add create file and folder

--- dash/urls.py
+++ dash/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index.dashboard_view, name='dashboard'),
     path('login/', views.auth.login_view, name='login'),
     path('logout/', views.auth.logout_view, name='logout'),
+    path('action/create_file/', views.index.create_file_view, name='create_file'),
 ]
--- dash/views/index.py
+++ dash/views/index.py
@@ -2,12 +2,56 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
+from files.models import Items
+
 
 def dashboard_view(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
 
+    items = Items.objects.filter(owner=request.user, is_deleted=False)
 
     return render(request, 'dash.html', {
+        "user": request.user,
+        "items": items,
+    })
+
+
+def create_file_view(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+
+    if request.method == 'POST':
+        item = Items()
+        item.name = request.POST.get('name')
+        item.content = request.POST.get('content')
+        item.is_file = True
+        if request.POST.get('parent'):
+            item.parent = Items.objects.get(pk=request.POST.get('parent'))
+        item.owner = request.user
+        item.save()
+        return HttpResponseRedirect(reverse('dashboard'))
+
+    return render(request, 'file_new.html', {
+        "user": request.user,
+    })
+
+
+def create_folder_view(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+
+    if request.method == 'POST':
+        item = Items()
+        item.name = request.POST.get('name')
+        item.content = request.POST.get('content')
+        item.is_file = False
+        if request.POST.get('parent'):
+            item.parent = Items.objects.get(pk=request.POST.get('parent'))
+        item.owner = request.user
+        item.save()
+        return HttpResponseRedirect(reverse('dashboard'))
+
+    return render(request, 'file_new.html', {
         "user": request.user,
     })
